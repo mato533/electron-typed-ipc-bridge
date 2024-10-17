63ef3c020b539de08cd59f08619dd0ce21fa5d28
mato533 <35281743+mato533@users.noreply.github.com>
2024-10-17 14:20:23 +0900

    docs: add jsdocs to index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,14 +2,32 @@ import type { IpcContextBridgeApi } from '../preload'
 import type { IpcBridgeApiSenderTypeGenerator } from '../main'
 import type { IpcBridgeApiImplementation } from '../channel'
 
+/**
+ * Type generator for api that will be exposed to renderer process
+ * Use at the preload sctipt
+ */
 export type { IpcBridgeApiTypeGenerator } from '../preload'
 
+/**
+ * Type generator for api to send message from main to renderer
+ * Use at the main process
+ */
 export type { IpcBridgeApiSenderTypeGenerator } from '../main'
 
+/**
+ * Resister IPC handler(for tow way)
+ * and generate api for send message to renderer process
+ * Use at the main process
+ * @param ipcBridgeApi Implementation for IPC api handlers
+ */
 export function registerIpcHandler<T extends IpcBridgeApiImplementation>(
   ipcBridgeApi: T
 ): IpcBridgeApiSenderTypeGenerator<T>
 
+/**
+ * Generate IPC api that will be exposed to renderer process.
+ * Use at the preload sctipt
+ */
 export function getApiInvoker<
   T extends IpcContextBridgeApi<IpcBridgeApiImplementation>,
 >(): Promise<T>

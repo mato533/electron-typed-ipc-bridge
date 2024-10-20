381daa2a21eddc3da047c6094b82476dca4c0b79
mato533 <35281743+mato533@users.noreply.github.com>
2024-10-21 00:03:31 +0900

    chore: migrate configration to eslint v9 at playground

---
+++ playground/eslint.config.js
@@ -0,0 +1,58 @@
+/* eslint-env node */
+import js from '@eslint/js'
+import prettierConfig from '@vue/eslint-config-prettier'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+  prettierConfig,
+  {
+    files: [
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.vue'
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false
+        }
+      ],
+      'vue/require-default-prop': 'off',
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  },
+  {
+    ignores: ['node_modules/*', 'dist/*', 'out/*', '**/*/.gitignore', 'coverage/*']
+  }
+]

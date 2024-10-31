ff3993a71d8e27a54574b900443d8464b9b3b8b5
mato533 <35281743+mato533@users.noreply.github.com>
2024-11-01 04:58:33 +0900

    refactor: reduce cyclomatic complexity of haveSameStructure

---
+++ src/__tests__/checker.spec.ts
@@ -0,0 +1,215 @@
+import { checkFalsy, haveSameStructure } from '../utils/checker'
+
+import type { IpcMainInvokeEvent } from 'electron'
+
+describe('checkFalsy', () => {
+  const api = {
+    on: { api: () => 1 },
+    invoke: { api: () => 1 },
+  }
+  const channelMap = {
+    on: { api: 'uuid' },
+    invoke: { api: 'uuid' },
+  }
+
+  it('ok', () => {
+    expect(checkFalsy(api, channelMap)).toBeTruthy()
+  })
+  it('ng1', () => {
+    expect(checkFalsy(api, undefined)).toBeFalsy()
+  })
+  it('ng2', () => {
+    expect(checkFalsy(undefined, channelMap)).toBeFalsy()
+  })
+})
+
+const apiHandlers = {
+  invoke: {
+    fn1: (e: IpcMainInvokeEvent) => console.log(e),
+    fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    name1: {
+      fn1: () => console.log(),
+      fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    },
+  },
+  on: {
+    fn1: () => console.log('sss'),
+    name2: {
+      fn1: () => console.log(),
+      fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    },
+  },
+} as const
+
+const apiHandlersOnlyInvoke = {
+  invoke: {
+    fn1: (e: IpcMainInvokeEvent) => console.log(e),
+    fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    name1: {
+      fn1: () => console.log(),
+      fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    },
+  },
+} as const
+
+const apiHandlersOnlyOn = {
+  on: {
+    fn1: () => console.log('sss'),
+    name2: {
+      fn1: () => console.log(),
+      fn2: (e: IpcMainInvokeEvent) => console.log(e),
+    },
+  },
+} as const
+describe('haveSameStructure', () => {
+  describe('success', () => {
+    it('on and invoke', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: {
+            fn1: 'uuid-name1-fn1',
+            fn2: 'uuid-name1-fn2',
+          },
+        },
+        on: {
+          fn1: 'uuid-fn1',
+          name2: {
+            fn1: 'uuid-name2-fn1',
+            fn2: 'uuid-name2-fn2',
+          },
+        },
+      }
+      expect(haveSameStructure(apiHandlers, channelMap)).toBeTruthy()
+    })
+
+    it('only invoke', () => {
+      const channelMap = {
+        on: {
+          fn1: 'uuid-fn1',
+          name2: {
+            fn1: 'uuid-name2-fn1',
+            fn2: 'uuid-name2-fn2',
+          },
+        },
+      }
+      expect(haveSameStructure(apiHandlersOnlyOn, channelMap)).toBeTruthy()
+    })
+
+    it('only on', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: {
+            fn1: 'uuid-name1-fn1',
+            fn2: 'uuid-name1-fn2',
+          },
+        },
+      }
+      expect(haveSameStructure(apiHandlersOnlyInvoke, channelMap)).toBeTruthy()
+    })
+  })
+  describe('fail', () => {
+    it('function missing', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: {
+            fn1: 'uuid-name1-fn1',
+            // fn2: 'uuid-name1-fn2', // unmatch
+          },
+        },
+        on: {
+          fn1: 'uuid-fn1',
+          name2: '12',
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlers, channelMap)).toBeFalsy()
+    })
+
+    it('object missing', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          // name1: {
+          //   fn1: 'uuid-name1-fn1',
+          //   fn2: 'uuid-name1-fn2', // unmatch
+          // },
+        },
+        on: {
+          fn1: 'uuid-fn1',
+          name2: '12',
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlers, channelMap)).toBeFalsy()
+    })
+
+    it('mismatch function name', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: {
+            fn1: 'uuid-name1-fn1',
+            fn99: 'uuid-name1-fn2', // unmatch
+          },
+        },
+        on: {
+          fn1: 'uuid-fn1',
+          name2: '12',
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlers, channelMap)).toBeFalsy()
+    })
+
+    it('mismatch object name', () => {
+      const channelMap = {
+        invoke: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name99: {
+            fn1: 'uuid-name1-fn1',
+            fn2: 'uuid-name1-fn2', // unmatch
+          },
+        },
+        on: {
+          fn1: 'uuid-fn1',
+          name2: '12',
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlers, channelMap)).toBeFalsy()
+    })
+
+    it('only Invoke', () => {
+      const channelMap = {
+        on: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: 'uuid-name1-fn3', // unmatch
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlersOnlyInvoke, channelMap)).toBeFalsy()
+    })
+
+    it('only On', () => {
+      const channelMap = {
+        on: {
+          fn1: 'uuid-fn1',
+          fn2: 'uuid-fn2',
+          name1: 'uuid-name1-fn3', // unmatch
+        },
+      }
+      // @ts-expect-error
+      expect(haveSameStructure(apiHandlersOnlyOn, channelMap)).toBeFalsy()
+    })
+  })
+})

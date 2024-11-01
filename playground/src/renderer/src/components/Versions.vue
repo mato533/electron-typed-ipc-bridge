0f72b5154c36c81233fc2a27face7c8e5c50a4cc
mato533 <35281743+mato533@users.noreply.github.com>
2024-11-01 09:21:38 +0900

    docs: reset default source code of Version.vue

--- playground/src/renderer/src/components/Versions.vue
+++ playground/src/renderer/src/components/Versions.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-const versions = ref()
-onMounted(() => {
-  const _versions = window.electron.process.versions
-  console.log(_versions)
-  versions.value = { ..._versions }
-})
+import { reactive } from 'vue'
+
+const versions = reactive({ ...window.electron.process.versions })
 </script>
 
 <template>
-  <ul v-if="versions" class="versions">
+  <ul class="versions">
     <li class="electron-version">Electron v{{ versions.electron }}</li>
     <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
     <li class="node-version">Node v{{ versions.node }}</li>
